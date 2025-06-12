5f73ef393f4411a2637afaa15b2c75e7c22387c6
caio <caio.pecly2018@gmail.com>
2025-06-11 22:41:08 -0300

    mudanças

--- components/Rotas.js
+++ components/Rotas.js
@@ -5,15 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './Home';
 import Alterar from './Alterar';
 import Cadastro from './Cadastro'; // sua outra tela de cadastro
-import Auth from './Auth'; // tela de Login + Cadastro que criamos
+
 
 const Stack = createNativeStackNavigator();
 
 export default function Rotas() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Auth">
-        <Stack.Screen name="Auth" component={Auth} options={{ title: 'Login / Cadastro' }} />
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Alterar" component={Alterar} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
